e6c25baf46b0005486a8620adb4467af3c6486cd
Eric Thill <code@thill.io>
2019-01-27 15:25:23 -0600

    Per File License

--- .idea/fileTemplates/includes/LICENSE.java
+++ .idea/fileTemplates/includes/LICENSE.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2019 Eric Thill
+ * Copyright (c) 2019 ${USER}
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
@@ -13,41 +13,3 @@
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package io.thill.trakrj.trackers;
-
-import io.thill.trakrj.Record;
-import io.thill.trakrj.Tracker;
-
-/**
- * Tracker to aggregate long values, reset to 0.
- *
- * @author Eric Thill
- */
-public class AggregateLongTracker implements Tracker {
-
-	private long value;
-
-	@Override
-	public void record(Record record) {
-		value += record.getValueLong();
-	}
-
-	@Override
-	public void reset() {
-		value = 0;
-	}
-	
-	@Override
-	public String toString() {
-		return String.valueOf(value);
-	}
-
-	/**
-	 * Get the aggregate
-	 *
-	 * @return The aggregate
-	 */
-	public long getAggregate() {
-		return value;
-	}
-}
